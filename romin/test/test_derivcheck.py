ae1f939a4dcc1f76f63c846858247eaf5c681938
Michael Richer <msricher1993@gmail.com>
2015-12-12 04:06:54 -0500

    Added unit test to verify weighted searching in deriv_check()
    1. Added a test that defines a function and gradient with indeterminate regions
    
    2. Made romin check the derivative near the indeterminate region, verified that
       it raises the appropriate error
    
    3. Reduced the weight of searching in the problematic direction; the check
       passes cleanly

--- romin/test/test_derivcheck.py
+++ romin/test/test_derivcheck.py
@@ -20,6 +20,8 @@
 #pylint: skip-file
 
 
+from exceptions import FloatingPointError
+from nose.tools import assert_raises
 from romin import *
 from romin.test.random_seed import numpy_random_seed
 
@@ -87,3 +89,25 @@ def test_deriv_check_nd_zeros():
     yield check_deriv_check_nd, 1, (3, 4)
     yield check_deriv_check_nd, 10, (10, )
     yield check_deriv_check_nd, 10, (3, 4)
+
+
+def check_deriv_check_weights():
+    # function is indeterminate for x[0] <= 1
+    def f(x):
+        with np.errstate(divide='raise'):
+            return x[1]/max(0, x[0] - 1) + x[2]
+    # gradient is indeterminate for x[0] <= 1
+    def g(x):
+        with np.errstate(divide='raise'):
+            return np.array([-x[1]/((x[0] - 1)**2), 1/max(0, x[0] - 1), 1.0])
+    # do searches near the indeterminate region
+    xs = np.array([1.03, 4.0, 1.0])
+    # romin searches into x[0] < 1
+    assert_raises(FloatingPointError, deriv_check, f, g, xs, 0.1, 16)
+    # reduce weight on x[0] so that romin does not search so far
+    weights = np.array([1.e-4, 1.0, 1.0])
+    deriv_check(f, g, xs, 0.1, 16, weights=weights)
+
+
+def test_deriv_check_nd_weights():
+    yield check_deriv_check_weights()
