29dc3dd278020b985e7f62fde961946b56f7a5d6
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-02-07 07:55:18 +0100

    Basic setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# RoMin is a robust minimizer.
+# Copyright (C) 2011-2015 Toon Verstraelen
+#
+# This file is part of RoMin.
+#
+# RoMin is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# RoMin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#--
+
+
+from distutils.core import setup
+
+setup(
+    name='romin',
+    version='0.0',
+    description='RoMin is a robust minimizer.',
+    package_dir = {'romin': 'romin'},
+    packages=['romin', 'romin/test'],
+)
